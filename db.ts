e17de6f3c650b6e3c397cbdf0947cad022970acc
levtrilev <levtrishankov@yandex.ru>
2025-11-18 17:00:55 +0300

    connection to my site

--- db.ts
+++ db.ts
@@ -1,7 +1,9 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
+  host: process.env.DB_HOST_MYSITE,
+  // host: process.env.DB_HOST_127,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
