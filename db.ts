5532b52e63f103e427c357d2d3e6ab5c2e1f55fd
levtrilev <levtrishankov@yandex.ru>
2026-01-13 00:00:31 +0300

    connect to NEON db

--- db.ts
+++ db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+const pool_bak = new Pool({
   // host: process.env.DB_HOST,
   host: process.env.DB_HOST_127,
   // host: process.env.DB_HOST_MYSITE,
@@ -10,6 +10,18 @@ const pool = new Pool({
   port: 5432, // порт PostgreSQL по умолчанию
   max: 20, // максимальное количество клиентов в пуле
   idleTimeoutMillis: 300000, // время ожидания перед закрытием неактивного соединения
+});
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  // host: process.env.POSTGRES_URL,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  port: 5432, // порт PostgreSQL по умолчанию
+  max: 20, // максимальное количество клиентов в пуле
+  idleTimeoutMillis: 300000, // время ожидания перед закрытием неактивного соединения
+  ssl: { rejectUnauthorized: true },
 });
 
 //   sslmode: require,
